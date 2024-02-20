1fb4754f88ba563e19b6701f85e01682df3987db
Dieter Whittingham <dieter.whitt@gmail.com>
2024-02-20 01:40:28 -0500

    nodemailer set up need to pay for forwardmail hosting plan

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const subscriberRoutes = require('./routes/subscribers');
 
+const nodeMailer = require('nodemailer');
+
 // express app
 const app = express();
 
@@ -39,4 +41,33 @@ mongoose.connect(process.env.MONGO_URI)
         console.log(error);
     });
 
+const message = `
+    <h1>hello from nodemailer!</h1>
+`;
+const recipient = '';
 
+async function send(){
+    // send email
+    const transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.MAILER_USER,
+            pass: process.env.MAILER_PASS
+        }
+    });
+
+    const info = await transporter.sendMail({
+        from: 'sundial <>',
+        to: recipient,
+        subject: 'Daily Sundial Update',
+        html: message,
+    })
+    // log info
+    console.log("message sent: " + info.messageId);
+}
+
+//send()
+//catch error
+//.catch(e => console.log(e));
