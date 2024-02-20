da8d479c6a984d657310c01d49ae11f1d51fd91d
Dieter Whittingham <dieter.whitt@gmail.com>
2024-02-19 19:52:49 -0500

    fetching sun data complete next working on frontend subscriptions, email db/backend, and ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function RenderTimes(){
   const [offset, setOffset] = useState(300);
   const [time, setTime] = useState(0);
   const [timezone, setTimezone] = useState('Eastern Standard Time');
+  const [sunData, setSunData] = useState({});
 
   //useEffect which attempts to get the user's location once upon initial render
   useEffect(() => { 
@@ -38,6 +39,8 @@ function RenderTimes(){
     // if error
     function error() {
       console.log('Unable to retrieve your location');
+      // set defualt coords
+      setCoords([43.6532,-79.3832]);
     }
   }, []);
 
@@ -64,7 +67,7 @@ function RenderTimes(){
         const newCity = `${data.city}, 
         ${data.countryName}`;
 
-        setCityName(newCity);
+        setCityName(newCity);  
       })
       .catch(error => {
         console.error('error trying to fetch: ', error);
@@ -75,7 +78,39 @@ function RenderTimes(){
   // coordinates change
   // uses the sunrisesunset.io api
   useEffect(() => {
-    return;
+    // get api response
+    const url = 'https://api.sunrisesunset.io/json?';
+    const location = `lat=${coords[0]}&lng=${coords[1]}`
+    // fetching json
+    console.log('attempting to fetch from ' + url + location);
+    fetch(url+location)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('network response was not ok');
+        }
+        // parse the json in the response
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        // attempt to access json fields
+        // update offset and sunrise/sunset times
+        const newOffset = -1 * data.results.utc_offset;
+        const newSunData = {
+          sunrise: data.results.sunrise,
+          sunset: data.results.sunset,
+          firstLight: data.results.first_light,
+          lastLight: data.results.last_light,
+        };
+        console.log(newOffset);
+        console.log(newSunData);
+        // setting
+        setOffset(newOffset);
+        setSunData(newSunData);
+      })
+      .catch(error => {
+        console.error('error trying to fetch: ', error);
+      });
   }, [coords]);
 
   // function that updates the selected time and timezone based on the 
@@ -105,6 +140,7 @@ function RenderTimes(){
       <br/>
       {offset} {time} {timezone}
       <br/>lat: {coords[0]} long: {coords[1]}
+      <br/>sun data: {Object.values(sunData)}
       </div>
   )
 
